2ce55a3bc015594c5710f133f76e228210bc0089
karl <karlhadwen@gmail.com>
2019-08-05 23:09:23 +0100

    refactor: firebase.prod - rename to use

---
+++ src/firebase.prod.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+});
+
+export { firebaseConfig as firebase };
