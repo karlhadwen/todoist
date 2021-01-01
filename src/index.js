65a58930b7e2b17a87c0f6ff51ef5615608d9a34
isa duvan <isa.duvan@paycore.com>
2021-01-01 20:30:13 +0300

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 import { App } from './App';
 import './App.scss';
 
-render(<App />, document.getElementById('root'));
+render(<App /> , document.getElementById('root'));
